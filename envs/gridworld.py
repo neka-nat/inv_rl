842a0c077da45b36606b5820d692760056647750
PabloAMC <pmorenocf@alumnos.unex.es>
2019-05-01 16:56:32 +0200

    Including more explanatory comments

--- envs/gridworld.py
+++ envs/gridworld.py
@@ -35,23 +35,31 @@ class GridworldEnv(discrete.DiscreteEnv):
 
         self.shape = shape
 
-        nS = np.prod(shape)
-        nA = 4
+        nS = np.prod(shape) # number of states
+        nA = 4              # number of actions
 
-        MAX_Y = shape[0]
-        MAX_X = shape[1]
+        MAX_Y = shape[0]    # shape of the gridworld, x direction
+        MAX_X = shape[1]    # shape of the gridworld, y direction
 
-        P = {}
+        P = {}              # dictionary of [states] [actions] = ([1.0, next_state, reward, is_done])
         grid = np.arange(nS).reshape(shape)
-        it = np.nditer(grid, flags=['multi_index'])
+        it = np.nditer(grid, flags=['multi_index']) # iteration over array 'grid'
+        
+        '''
+        Numeration of the matrix 4x4 is as follows:
+        0 1 2 3
+        4 5 6 7
+        8 9 10 11
+        12 23 14 15
+        '''
 
         while not it.finished:
-            s = it.iterindex
+            s = it.iterindex                    # states
             y, x = it.multi_index
 
-            P[s] = {a : [] for a in range(nA)}
+            P[s] = {a : [] for a in range(nA)}  # dictionary with info of state, action and reward
 
-            is_done = lambda s: s == 0 or s == (nS - 1)
+            is_done = lambda s: s == 0 or s == (nS - 1) #can be modified to include more goals
             reward = 0.0 if is_done(s) else -1.0
 
             # We're stuck in a terminal state
@@ -61,10 +69,10 @@ class GridworldEnv(discrete.DiscreteEnv):
                 P[s][DOWN] = [(1.0, s, reward, True)]
                 P[s][LEFT] = [(1.0, s, reward, True)]
             # Not a terminal state
-            else:
-                ns_up = s if y == 0 else s - MAX_X
+            else:           #One may want to include some kind of list of goal states to substitute the next four lines.
+                ns_up = s if y == 0 else s - MAX_X # move one full row to the left
                 ns_right = s if x == (MAX_X - 1) else s + 1
-                ns_down = s if y == (MAX_Y - 1) else s + MAX_X
+                ns_down = s if y == (MAX_Y - 1) else s + MAX_X # move one full row to the right
                 ns_left = s if x == 0 else s - 1
                 P[s][UP] = [(1.0, ns_up, reward, is_done(ns_up))]
                 P[s][RIGHT] = [(1.0, ns_right, reward, is_done(ns_right))]
